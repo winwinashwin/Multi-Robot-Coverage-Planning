324a5e9922bc4e015a02ae51c48ec069b9e6d8ec
Ashwin A Nayar <ashwin5059198@gmail.com>
2021-11-13 00:12:43 +0530

    [lint] black

--- src/full_coverage_path_planner/coverage_maps/merge.py
+++ src/full_coverage_path_planner/coverage_maps/merge.py
@@ -4,10 +4,14 @@ import cv2
 import numpy
 import numpy as np
 
-grids = [cv2.imread('cov0.pgm', cv2.IMREAD_GRAYSCALE), cv2.imread('cov1.pgm', cv2.IMREAD_GRAYSCALE),
-         cv2.imread('cov2.pgm', cv2.IMREAD_GRAYSCALE), cv2.imread('cov3.pgm', cv2.IMREAD_GRAYSCALE)]
+grids = [
+    cv2.imread("cov0.pgm", cv2.IMREAD_GRAYSCALE),
+    cv2.imread("cov1.pgm", cv2.IMREAD_GRAYSCALE),
+    cv2.imread("cov2.pgm", cv2.IMREAD_GRAYSCALE),
+    cv2.imread("cov3.pgm", cv2.IMREAD_GRAYSCALE),
+]
 
-original = cv2.imread('map4_small.pgm', cv2.IMREAD_GRAYSCALE)[10:210, 10:210]
+original = cv2.imread("map4_small.pgm", cv2.IMREAD_GRAYSCALE)[10:210, 10:210]
 
 merged = 0 * grids[0] + 255
 
@@ -17,9 +21,9 @@ for g in grids:
 check = cv2.bitwise_and(merged, original)
 arr = np.array(check)
 counts = dict(zip(*numpy.unique(arr, return_counts=True)))
-print(counts[0]/(counts[0] + counts[254]))
+print(counts[0] / (counts[0] + counts[254]))
 
-cv2.imshow('merged', check)
+cv2.imshow("merged", check)
 
 
 cv2.waitKey(0)
