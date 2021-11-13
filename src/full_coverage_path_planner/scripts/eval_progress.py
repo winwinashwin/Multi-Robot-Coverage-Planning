cac1d7e0207436893ab23bb381c86413267a4853
Ashwin A Nayar <ashwin5059198@gmail.com>
2021-11-13 19:25:32 +0530

    [lint] black

--- src/full_coverage_path_planner/scripts/eval_progress.py
+++ src/full_coverage_path_planner/scripts/eval_progress.py
@@ -14,31 +14,39 @@ import numpy as np
 
 class CoverageProgressEvaluatorNode:
     def __init__(self):
-        self._static_map_topic = rospy.get_param('~static_map_topic')
-        self._cov_map_topic = rospy.get_param('~coverage_map_topic')
+        self._static_map_topic = rospy.get_param("~static_map_topic")
+        self._cov_map_topic = rospy.get_param("~coverage_map_topic")
 
-        self._static_map = rospy.wait_for_message(self._static_map_topic, OccupancyGrid)
-        self._map_subscription = rospy.Subscriber(self._cov_map_topic, OccupancyGrid, self.map_callback)
+        self._static_map = rospy.wait_for_message(
+            self._static_map_topic, OccupancyGrid
+        )
+        self._map_subscription = rospy.Subscriber(
+            self._cov_map_topic, OccupancyGrid, self.map_callback
+        )
 
-        self._progress_publisher = rospy.Publisher('coverage_progress', Float64, queue_size=5)
+        self._progress_publisher = rospy.Publisher(
+            "coverage_progress", Float64, queue_size=5
+        )
 
     def map_callback(self, coverage_map: OccupancyGrid):
         static_grid = np.array(self._static_map.data)
         coverage_grid = np.array(coverage_map.data)
         assert static_grid.shape == coverage_grid.shape
 
-        counts = dict(zip(*np.unique(coverage_grid[static_grid != -1], return_counts=True)))
+        counts = dict(
+            zip(*np.unique(coverage_grid[static_grid != -1], return_counts=True))
+        )
         progress = 100 * counts[75] / (counts[0] + counts[75])
         self._progress_publisher.publish(progress)
 
 
 def main():
-    rospy.init_node('eval_progress', anonymous=True)
+    rospy.init_node("eval_progress", anonymous=True)
     _ = CoverageProgressEvaluatorNode()
     rospy.spin()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         main()
     except rospy.ROSInterruptException:
